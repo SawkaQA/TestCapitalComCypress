656a2048049001798f7339d5dcc15245a897c53b
SawkaQA <aleksandr.sdobnov.qa@gmail.com>
2023-06-04 23:16:15 +0300

    done: US_11.03.05_Swing_Platform

--- cypress/e2e/US_11_Education/US_11.03.05_Education_Swing_Trading.cy.js
+++ cypress/e2e/US_11_Education/US_11.03.05_Education_Swing_Trading.cy.js
@@ -5,6 +5,7 @@ import { bannerBtn } from "../../pageObjects/BannerButtons";
 import Header from "../../pageObjects/Header";
 import Login from "../../pageObjects/SignupLogin/Login";
 import Signup from "../../pageObjects/SignupLogin/SignUp"
+import links from "../../fixtures/links.json";
 const header = new Header();
 const login = new Login();
 const signUp = new Signup();
@@ -99,10 +100,53 @@ describe("US_11.03.05 | Educations > Menu item [Swing Trading]", () => {
         signUp.clickCloseSignUpFormBtn();
     })
 
-    it.only('TC_11.03.05_05 | Educations > Menu item [Swing trading] > Test buttons [Trade] on Widget "Most traded"', () => {
+    it('TC_11.03.05_05 | Educations > Menu item [Swing trading] > Test buttons [Trade] on Widget "Most traded"', () => {
         header.getEducationMenu().realHover();
         basePage.clickSwingTradingOnEducationMenu();
         bannerBtn.clickTradeBtnOnWidgetMostTraded();
+
+        signUp.getFormSignUp().should("be.visible");
+        signUp.getHeaderNameSignUp().should('have.text', 'Sign up');
+        signUp.getLoginLinkForm().should('be.visible')
+            .and('have.text', 'Login');
+        signUp.getEmail().should('be.visible')
+            .and('have.attr', 'type', 'email');
+        signUp.getPassword().should('be.visible')
+            .and('have.attr', 'type', 'password');
+        signUp.getBtnContinue()
+            .should('be.visible')
+            .and("have.text", "Continue");
+        signUp.getPolicyLink().should('be.visible')
+            .and('have.text', 'Privacy Policy');
+        signUp.clickCloseSignUpFormBtn();
+    })
+
+    it('TC_11.03.05_06 | Education > Menu Item [Swing Trading] > Test button [Download on the App Store] in the block "Sign up and trade smart today"', () => {
+        header.getEducationMenu().realHover();
+        basePage.clickSwingTradingOnEducationMenu();
+        bannerBtn.clickDownloadOnAppStoreBtn();
+        cy.url().should('eq', links.downloadAppStoreLink)
+    })
+
+    it('TC_11.03.05_07 | Education > Menu Item [Swing Trading] > Test button [Get it on Google Play] in the block "Sign up and trade smart today"', () => {
+        header.getEducationMenu().realHover();
+        basePage.clickSwingTradingOnEducationMenu();
+        bannerBtn.clickDownloadOnGooglePlayLink();
+        cy.url().should('eq', links.downloadAppStoreLink)
+
+    })
+
+    it('TC_11.03.05_08 | Education > Menu Item [Swing Trading] > Test button [Explore Web Platform] in the block "Sign up and trade smart today"', () => {
+        header.getEducationMenu().realHover();
+        basePage.clickSwingTradingOnEducationMenu();
+        bannerBtn.clickExploreWebPlatformLink();
+        cy.url().should('eq', links.exploreWebPlatformLink)
+    })
+
+    it('TC_11.03.05_09 | Education > Menu Item [Swing Trading] > Test button [Create & verify your account] in the block "Still looking for a broker you can trust?"', () => {
+        header.getEducationMenu().realHover();
+        basePage.clickSwingTradingOnEducationMenu();
+        bannerBtn.clickCreateAndVerifyBtn();
 
         signUp.getFormSignUp().should("be.visible");
         signUp.getHeaderNameSignUp().should('have.text', 'Sign up');
--- cypress/pageObjects/BannerButtons.js
+++ cypress/pageObjects/BannerButtons.js
@@ -2,12 +2,35 @@ export class BannerButtons {
 
     getStartTradingBtnOnMainBanner = () => cy.get('.btn--darkText');
     getTryDemoBtnOnMainBanner = () => cy.get('.btn--emptyblack');
-    getTradeBtnOnWidgetMostTraded = () => cy.get('.mostTraded__box--expanded > .mostTraded__market > .mostTraded__btn')
+    getTradeBtnOnWidgetMostTraded = () => cy.get('a[href="/trading/platform/spotlight/27045129890124996?side=buy"]')
+
+    // Footer
+    getDownloadOnAppStoreBtn = () => cy.get('.store-apple');
+    getDownloadOnGooglePlayLink = () => cy.get('.store-google')
+    getExploreWebPlatformLink = () => cy.get('.badge-platform')
+    getCreateAndVerifyBtn = () => cy.get('.regSteps__shape > .js_signup')
+
 
     // methods
     clickStartTradingBtnOnMainBanner() {
         this.getStartTradingBtnOnMainBanner().realHover().click()
         return this;
+    }
+
+    clickDownloadOnAppStoreBtn() {
+        this.getDownloadOnAppStoreBtn().realHover().click();
+    }
+
+    clickCreateAndVerifyBtn() {
+        this.getCreateAndVerifyBtn().click();
+    }
+
+    clickDownloadOnGooglePlayLink() {
+        this.getDownloadOnGooglePlayLink().realHover().click();
+    }
+
+    clickExploreWebPlatformLink() {
+        this.getExploreWebPlatformLink().realHover().click();
     }
 
     clickTryDemoBtnOnMainBanner() {
