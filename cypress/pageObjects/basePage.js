0bb502a7a1656e316b418141403fe2528134eabf
Alexandr Sdobnov <126351854+SawkaQA@users.noreply.github.com>
2023-06-07 20:57:39 +0300

    Check ci (#14)
    * fix: us11_03.05_swing_trading

--- cypress/pageObjects/basePage.js
+++ cypress/pageObjects/basePage.js
@@ -6,7 +6,6 @@ export class BasePage {
 
     // Education Menu
     // Trading strategies guide
-
     getPositionTrading = () => cy.get(`[class='grid gXs'] a[href="https://capital.com/position-trading"]`)
     getSwingTradingOnEducationMenu = () => cy.get('[class="cc-nav__dropdown gridRUp gXs "] [href="https://capital.com/swing-trading"]')
     // [class="cc-nav__dropdown gridRUp gXs "] 
