feef11ea108b5b00ca30b44597cd0e0523c0aca0
ElenaKanash <hird250@gmail.com>
2024-04-06 18:34:18 +0300

    add allure report

--- cypress/pageObjects/HeaderFCA.js
+++ cypress/pageObjects/HeaderFCA.js
@@ -6,6 +6,7 @@ class HeaderFCA {
   // MenuButtons
   getMarketsNenu = () => cy.get('#header  a[href="/en-gb/markets"]');
   getSharesMarketsNenu = () => cy.get('#header a[href="/en-gb/markets/shares"]');
+  getSharesMarketsHeading  = () => cy.get('h1');
 
 
   //Methods
@@ -14,9 +15,12 @@ class HeaderFCA {
   }
 
   openSharesMarketsNenu() {
-    this.getSharesMarketsNenu().click({ force: true }).wait(25000);
+    this.getSharesMarketsNenu().click({ force: true });
+    //cy.wait(25000);
+    this.getSharesMarketsHeading().should('be.visible');   
     cy.url().should('contain', headerData.endPointsMenuMarketsFCA.marketsShares);
   }
+  
 
 }
 
